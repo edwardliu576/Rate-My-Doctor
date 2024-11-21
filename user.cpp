6c39f89a6defdb4b22a9c27c8b7aaa3062cde3c4
edwardliu576 <edwardliu373@gmail.com>
2024-11-21 14:39:19 -0800

    created basics of user class

---
+++ user.cpp
@@ -0,0 +1,3 @@
+#include "user.hpp"
+
+User::User(const string &n, const string &un, const string &pwd, const string &l): Account(n, un, pwd, l){}
---
+++ user.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <vector>
+//#include <doctor.h>             update to get correct paths later
+//#include <appointment.h>        update to get correct paths later
+#include "account.h"
+using namespace std;
+
+class User : public Account{
+    private:
+        vector<Doctor> favoriteDoctors(10);   //default size of 10
+        vector<Appointment> appointments(10); //default size of 10
+        void calculateDistance();     //this will be figured out later
+    public:
+        User(const string &, const string &, const string &, const string &);
+
+        //these are put in place to know what to do, but the details
+        //such as return type and parameters will be figured out later
+        //as development progresses
+        void leaveReview();
+        void editReview();
+        void deleteReview();
+        void scheduleAppointment();
+        void editAppointment();
+        void deleteAppointment();
+        void viewAppointments();
+        void viewSuggestedDoctors();
+        void addToFavorites();
+
+};
