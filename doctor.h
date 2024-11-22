ed558998a2531a93144235d3b259e5f6a18bba1e
nicholegodfrey <nicholegodfrey10@gmail.com>
2024-11-21 21:49:34 -0800

    made changes

--- doctor.h
+++ doctor.h
@@ -1,11 +1,11 @@
 #pragma once
 //still need to include account class
-//#include "account.h"
+#include "account.h"
 #include <string>
 
 using namespace std;
 
-class Doctor {
+class Doctor: public Account {
     string name;
     string username;
     string password;
@@ -17,7 +17,7 @@ class Doctor {
     string hospital;
 
     Doctor(const string &n, const string &un, const string &pwd, const string &l, const string &spec, double &rate, string &hosp) : 
-        name(n), username(un), password(pwd), location(l), specialty(spec),rating(rate), hospital(hosp) {};
+        Account(n, un, pwd, l) {};
 
     void editSpecialty(const string &);
     void editHospital(const string &);
