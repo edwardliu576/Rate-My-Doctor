7f7be9623991aa081bbf5145dd6a3cbb7f4b0a19
edwardliu576 <edwardliu373@gmail.com>
2024-12-06 01:04:11 -0800

    updated user class by removing unnecessary stuff

--- user.hpp
+++ user.hpp
@@ -1,34 +1,16 @@
 #pragma once
 
 #include <vector>
-#include "doctor.h"            
-#include "appointment.h"       
+#include "doctor.h"                              
 #include "account.h"
 #include "ratings.h"
 using namespace std;
 
 class User : public Account{
     private:
-        vector<Doctor> favoriteDoctors;   
-        vector<Appointment> appointments;
-        void calculateDistance();     //this will be figured out later
+        vector<Doctor> favoriteDoctors;
     public:
-        User(const string &, const string &, const string &, const string &); //good!
-
-        void leaveReview(const Doctor &, double, const string&); //need to add friend functions/getters
-
-        void editReview(Doctor &, double, const string&); //to be decided
-        void deleteReview(); //to be decided
-
-        void scheduleAppointment(const Doctor &, const string &, const string &, bool); //good!
-
-        void editAppointment(); //to be decided
-        void deleteAppointment(); //to be decided
-
-        void viewAppointments(); //good! but remember friend class declaration
-
-        void viewSuggestedDoctors(); //need to figure out with calculateDistance too
-
-        void addToFavorites(const Doctor &); //good!
-
+        User(const string &n, const string &un, const string &pwd, const string &addy,const string &zip);
+        void addToFavorites(const Doctor &);
+        vector<Doctor> getFavoriteDoctors();
 };
